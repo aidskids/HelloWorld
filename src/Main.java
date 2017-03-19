cdd66c1973b6e0a729f5e1ff963b7c9a2cc8f9c3
gaohan <3456601322@qq.com>
2017-03-19 18:20:51 +0800

    简化冗余代码

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,17 @@ import org.junit.jupiter.api.Test;
 
 public class Main {
 
-    private static Main2 main2;
-
     public Main() {
-        main2 = new Main2();
-        System.out.println("1255553");
+        Main2 main2 = new Main2();
+        System.out.println(main2);
         System.out.println("1255553");
     }
-
-    public String velocity;
-
     private static void doSome() {
         System.out.println("Hello World!哈123123喽你妹！");
         System.out.println("efj12312312iejfie");
         System.out.println();
 
     }
-
-    public String getVelocity() {
-        return velocity;
-    }
-
-    public void setVelocity(String velocity) {
-        this.velocity = velocity;
-    }
-
     @Test
     public void haha() {
         doSome();
